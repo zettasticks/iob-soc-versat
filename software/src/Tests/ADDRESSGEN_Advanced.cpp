5d5ae63a834d1944873a8be74f4602c2a9fdedb2
z <ruben.teixeira@tecnico.ulisboa.pt>
2025-04-12 00:51:20 +0100

    Saving mostly the new implentation of address gen. Most tests that used old address gen are failing, need to integrate the new address gen

--- software/src/Tests/ADDRESSGEN_Advanced.cpp
+++ software/src/Tests/ADDRESSGEN_Advanced.cpp
@@ -1,11 +1,27 @@
 #include "testbench.hpp"
 
+#define MAX_SIZE 1000
+void ClearBuffer(int* buffer){
+   for(int i = 0; i < MAX_SIZE; i++){
+      buffer[i] = 0;
+   }
+}
+
+void CheckEfficiency(AddressVReadArguments args){
+   SimulateVReadResult sim = SimulateVRead(args);
+   float percent = ((float) sim.amountOfInternalValuesUsed) / ((float) sim.amountOfExternalValuesRead);
+   printf("Efficiency: %2f\n",percent);
+}
+
 void SingleTest(Arena* arena){
+   int expectedValues[] = {10,20,30,40,50,60,70,80};
    int buffer[1000] = {};
 
    int values = 8;
    ConfigureMemoryReceive(&accelConfig->output,values);
 
+{
+   ClearBuffer(buffer);
    buffer[0]  = 10;
    buffer[2]  = 20;
    buffer[4]  = 30;
@@ -15,14 +31,20 @@ void SingleTest(Arena* arena){
    buffer[9]  = 70;
    buffer[11] = 80;
 
-   Configure(&accelConfig->read,(iptr) buffer,2,4,2,5);
+   AddressVReadArguments compiled = CompileVUnit_AddressGenAdvancedTest((iptr) buffer,2,4,2,5);
+   LoadVUnit_VReadMultiple(&accelConfig->read,compiled);
+   CheckEfficiency(compiled);
+
    RunAccelerator(3);
    for(int i = 0; i < values; i++){
       printf("%d\n",VersatUnitRead(TOP_output_addr,i));
+      Assert_Eq(VersatUnitRead(TOP_output_addr,i),expectedValues[i]);
    }
    printf("\n");
+}
 
-
+{
+   ClearBuffer(buffer);
    buffer[0]   = 10;
    buffer[2]   = 20;
    buffer[4]   = 30;
@@ -32,31 +54,43 @@ void SingleTest(Arena* arena){
    buffer[104] = 70;
    buffer[106] = 80;
 
-   Configure(&accelConfig->read,(iptr) buffer,2,4,2,100);
+   AddressVReadArguments compiled = CompileVUnit_AddressGenAdvancedTest((iptr) buffer,2,4,2,100);
+   LoadVUnit_VReadMultiple(&accelConfig->read,compiled);
+   CheckEfficiency(compiled);
+
    RunAccelerator(3);
    for(int i = 0; i < values; i++){
       printf("%d\n",VersatUnitRead(TOP_output_addr,i));
+      Assert_Eq(VersatUnitRead(TOP_output_addr,i),expectedValues[i]);
    }
    printf("\n");
+}
 
-
+{
+   ClearBuffer(buffer);
    buffer[0]  = 10;
    buffer[5]  = 20;
-   buffer[4] = 30;
-   buffer[9] = 40;
+   buffer[4]  = 30;
+   buffer[9]  = 40;
    buffer[8]  = 50;
    buffer[13] = 60;
    buffer[12] = 70;
-   buffer[18] = 80;
+   buffer[17] = 80;
 
-   Configure(&accelConfig->read,(iptr) buffer,4,2,5,4);
+   AddressVReadArguments compiled = CompileVUnit_AddressGenAdvancedTest((iptr) buffer,4,2,5,4);
+   LoadVUnit_VReadMultiple(&accelConfig->read,compiled);
+   CheckEfficiency(compiled);
+
    RunAccelerator(3);
    for(int i = 0; i < values; i++){
       printf("%d\n",VersatUnitRead(TOP_output_addr,i));
+      Assert_Eq(VersatUnitRead(TOP_output_addr,i),expectedValues[i]);
    }
    printf("\n");
+}
 
-
+{
+   ClearBuffer(buffer);
    buffer[0]  = 10;
    buffer[5]  = 20;
    buffer[10] = 30;
@@ -66,13 +100,16 @@ void SingleTest(Arena* arena){
    buffer[12] = 70;
    buffer[17] = 80;
 
-   Configure(&accelConfig->read,(iptr) buffer,2,4,5,2);
+   AddressVReadArguments compiled = CompileVUnit_AddressGenAdvancedTest((iptr) buffer,2,4,5,2);
+   LoadVUnit_VReadMultiple(&accelConfig->read,compiled);
+   CheckEfficiency(compiled);
+
    RunAccelerator(3);
    for(int i = 0; i < values; i++){
       printf("%d\n",VersatUnitRead(TOP_output_addr,i));
+      Assert_Eq(VersatUnitRead(TOP_output_addr,i),expectedValues[i]);
    }
    printf("\n");
-
-
+}
 
 }
